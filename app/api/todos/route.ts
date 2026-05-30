28712516e2ef8f27135f6c6d3bfa635b637d6bf4
Vigneswari0806 <vigneswariyerri287@gmail.com>
2026-05-30 16:12:14 +0530

    fix dynamic routes

--- app/api/todos/route.ts
+++ app/api/todos/route.ts
@@ -1,3 +1,6 @@
+export const dynamic = 'force-dynamic'
+
+
 import { db } from '@/lib/db'
 import { auth } from '@/lib/auth'
 import { NextResponse } from 'next/server'
--- lib/db.ts
+++ lib/db.ts
@@ -4,6 +4,8 @@ const globalForPrisma = global as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const db = globalForPrisma.prisma ?? new PrismaClient()
+export const db = globalForPrisma.prisma ?? new PrismaClient({
+  log: ['error'],
+})
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
